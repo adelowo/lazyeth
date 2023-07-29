a7ad6661268fed4aa5b5bd4cfd130854a2abf631
Lanre Adelowo <yo@lanre.wtf>
2023-07-29 23:47:06 +0100

    add basic license and readme

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
 func main() {
+	app := &cli.App{
+		Name:  "lazyeth",
+		Usage: "Crypto wallet via the commandline",
+		Action: func(*cli.Context) error {
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
